7722c8f6e526e72eb728c239ae34353dd45b506a
raman22500 <raman@thirtyfour.in>
2018-09-18 16:23:32 +0530

    Initial commit

---
+++ Desktop/issues/OneSignalDemo/app/src/main/java/com/onesignal/onesignaldemo/AnotherDemoNoti.java
@@ -0,0 +1,13 @@
+package com.onesignal.onesignaldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnotherDemoNoti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_demo_noti);
+    }
+}
---
+++ Desktop/issues/OneSignalDemo/app/src/main/java/com/onesignal/onesignaldemo/GreenActivity.java
@@ -0,0 +1,4 @@
+package com.onesignal.onesignaldemo;
+
+class GreenActivity {
+}
